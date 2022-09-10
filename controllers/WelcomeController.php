32e9754046e3c05cbf986e1cdbcf9b7902a27370
Slava <erema_ssss@mail.ru>
2022-09-10 19:17:38 +0700

    add welcome layout

--- controllers/WelcomeController.php
+++ controllers/WelcomeController.php
@@ -6,6 +6,8 @@ class WelcomeController extends AppController
 {
     public function actionIndex()
     {
+        $this->layout = 'welcome';
+        $this->view->title = 'Главная страница';
         return $this->render('index');
     }
 }
